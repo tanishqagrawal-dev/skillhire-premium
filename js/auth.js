b5ca19a03bef4f43d34b9510508c4c5e7c761a98
Tanishq Agrawal <tanishqagrawal1112@gmail.com>
2026-01-06 22:35:17 +0530

    Add authentication functions for Google login and logout

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+
+function googleLogin() {
+  alert("Google Login Successful (Mock)");
+  window.location.href = "dashboard.html";
+}
+
+function logout() {
+  window.location.href = "index.html";
+}
