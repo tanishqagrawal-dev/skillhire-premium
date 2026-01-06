b7c2dff8e20c5b6935fd66aa70a4c9c52581de7f
Tanishq Agrawal <tanishqagrawal1112@gmail.com>
2026-01-06 23:05:42 +0530

    Initialize AOS and VanillaTilt on page load

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 
-console.log("SkillHire Platform Loaded");
+AOS.init({ duration: 1200 });
+
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+  max: 15,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.3
+});
